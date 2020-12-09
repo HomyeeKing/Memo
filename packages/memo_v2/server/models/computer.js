326e46193f0d001957b382d51f52f7e705876be8
HomyeeKing <wanghongye@kuaishou.com>
2020-12-09 19:20:28 +0800

    feat: hide server

--- packages/memo_v2/server/models/computer.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-const schema = new mongoose.Schema({
-    word: String,
-    phoSymbol: String,
-    translation: String
-})
-module.exports = Voca = mongoose.model("vocabularies", schema)
