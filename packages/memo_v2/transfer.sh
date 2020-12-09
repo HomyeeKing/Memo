08dcb0a3c842eb0213db24dca94ddf08dc7e9b88
Homyee King <49113249+HomyeeKing@users.noreply.github.com>
2020-12-09 19:38:11 +0800

    Delete transfer.sh

--- packages/memo_v2/transfer.sh
+++
@@ -1,9 +0,0 @@
-UPLOAD_DIR=$1
-if [ ! -d "${UPLOAD_DIR}" ] ; then 
- echo "There's not directory ${UPLOAD_DIR} in root";
- exit 1;
-fi 
-
-REMOTE_DIR=/root/memrise/;
-
-scp -r ${UPLOAD_DIR} root@121.36.32.150:${REMOTE_DIR}
